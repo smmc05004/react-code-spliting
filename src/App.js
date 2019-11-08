384d6bd372c6643076487031dead7cf0b4a6bcff
SEOKHWALEE <smmc05004@hanmail.net>
2019-11-08 22:57:54 +0900

    * simple code spliting complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, Suspense } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import loadable from "@loadable/component";
 
-function App() {
+// import notify from "./nofity";
+
+// const SplitMe = React.lazy(() => import("./SplitMe"));
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>
+});
+
+const App = () => {
+  // const onClick = () => {
+  //   // notify();
+  //   import("./nofity").then(result => result.default());
+  // };
+
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    console.log("moseOver");
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
       </header>
     </div>
   );
-}
+};
 
+// class App extends Component {
+//   state = {
+//     SplitMe: null
+//   };
+//   handleClick = async () => {
+//     const loadedModule = await import("./SplitMe");
+//     this.setState({
+//       SplitMe: loadedModule.default
+//     });
+//   };
+//   render() {
+//     const { SplitMe } = this.state;
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p onClick={this.handleClick}>Hello React!</p>
+//           {SplitMe && <SplitMe />}
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 export default App;
